29008b10dddbe527f87505b0bccfb4b7cd9c5c0d
мария <oops.mari@mail.ru>
2022-12-04 13:53:10 +0300

    Homework

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+void Cube(int[] num)
+{
+  int count = 0;
+  int length = num.Length;
+  while (count <  length)
+  {
+    num[count] = Convert.ToInt32(Math.Pow(count, 3));
+    count++;
+  }
+}
+
+void PrintArray(int[] coll)
+{
+  int count = coll.Length;
+  int index = 1;
+  while(index < count)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+int[] array = new int[num+1];
+Cube(array);
+PrintArray(array);

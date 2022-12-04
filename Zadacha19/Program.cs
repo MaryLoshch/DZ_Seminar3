6fe242adb4d8ee02eedf6ee7916b255b34ee7eeb
мария <oops.mari@mail.ru>
2022-12-04 12:46:41 +0300

    Задача 19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 99999 || num < 10000)
+{
+    Console.WriteLine("Вы ввели не пятизначное число. Попробуйте еще раз ");
+    return;
+}
+
+int n5 = num % 10;
+int n4 = num / 10 % 10;
+int n2 = num / 1000 % 10;
+int n1 = num / 10000 % 10;
+if (n5 == n1 && n4 == n2)
+{
+    Console.WriteLine("Да, это палиндром ");
+}
+else
+{
+    Console.WriteLine("Нет, это НЕ палиндром ");
+}
